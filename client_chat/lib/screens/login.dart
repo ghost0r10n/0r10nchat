c09f92a040b77201faf1abaaceb0e5d6f509aa18
semantoDevelopers <semantoDevelopers@gmail.com>
2020-09-14 12:36:20 +0200

    first push

---
+++ client_chat/lib/screens/login.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        decoration: BoxDecoration(
+          gradient: SweepGradient(
+    
+            colors: [Colors.green,Colors.black]
+          )
+        ),
+      ),
+    );
+  }
+}
